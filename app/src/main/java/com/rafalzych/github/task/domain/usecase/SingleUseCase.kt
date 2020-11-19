9c018d4383d0c7a3f047b7c2f04b5b9ac0b5132f
rafalzych <razy@ailleron.com>
2020-11-19 19:38:01 +0100

    add abstract simple SingleUseCase class

---
+++ app/src/main/java/com/rafalzych/github/task/domain/usecase/SingleUseCase.kt
@@ -0,0 +1,27 @@
+package com.rafalzych.github.task.domain.usecase
+
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+
+abstract class SingleUseCase<T, in Params> {
+
+    abstract fun createSingleUseCase(params: Params? = null): Single<T>
+
+    fun execute(subscriber: Subscriber<T>, params: Params? = null): Disposable {
+        return this.createSingleUseCase()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { subscriber.doOnSubscribe() }
+            .doFinally { subscriber.doFinally() }
+            .subscribe(subscriber::onSuccess, subscriber::onError)
+    }
+
+    abstract class Subscriber<T> {
+        abstract fun onSuccess(result: T)
+        abstract fun onError(exp: Throwable)
+        open fun doOnSubscribe() {}
+        open fun doFinally() {}
+    }
+}
