87cd669b375da7088e8a0c3a88e845d0dc1d9090
rafalzych <razy@ailleron.com>
2020-11-20 09:10:14 +0100

    add fragment stack check on backPressed

--- app/src/main/java/com/rafalzych/github/task/ui/MainActivity.kt
+++ app/src/main/java/com/rafalzych/github/task/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.rafalzych.github.task.R
 import com.rafalzych.github.task.ui.base.BaseContract
+import com.rafalzych.github.task.ui.base.BaseFragment
 import com.rafalzych.github.task.ui.navigation.Navigator
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -26,5 +27,32 @@ class MainActivity : AppCompatActivity(), BaseContract.ProgressView {
 
     override fun hideProgress() {
         vProgressView.hide()
+    }
+
+    override fun onBackPressed() {
+        if (getCurrentFragment() == null) {
+            super.onBackPressed()
+            return
+        }
+
+        if (supportFragmentManager.backStackEntryCount > 1) {
+            supportFragmentManager.popBackStack()
+        } else {
+            onSingleFragmentBackPressed()
+        }
+    }
+
+    private fun onSingleFragmentBackPressed() {
+        finish()
+    }
+
+    private fun getCurrentFragment(): BaseFragment<*>? {
+        if (supportFragmentManager.backStackEntryCount > 0) {
+            val currentFragmentIndex = supportFragmentManager.backStackEntryCount - 1
+            val currentFragmentName =
+                supportFragmentManager.getBackStackEntryAt(currentFragmentIndex).name
+            return supportFragmentManager.findFragmentByTag(currentFragmentName) as? BaseFragment<*>
+        }
+        return null
     }
 }
